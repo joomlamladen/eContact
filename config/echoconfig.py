00cef6fc95c61cfe04de70251db638ced92168f3
DigitalCube <info@digitalcube.rs>
2016-04-08 07:11:23 -0400

    Changes on servers files

--- config/echoconfig.py
+++ config/echoconfig.py
@@ -1,4 +1,4 @@
 
 from collections import namedtuple
 DbConfig = namedtuple('DbConfig', 'db, host, user, passwd, charset')
-db_config = DbConfig('dcube', 'localhost', 'tms', '123', 'utf8')
+db_config = DbConfig('digitalcube', 'localhost', 'econ', '123', 'utf8')
