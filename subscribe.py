0c533430978da7501a56fe9176157b7edf3af59c
Mladen <mladen@digitalcube.rs>
2016-04-26 19:13:20 +0200

    subscribe add project

--- subscribe.py
+++ subscribe.py
@@ -20,9 +20,10 @@ request_timeout = 10
     api_return=[(200, 'OK'), (404, '')]
 )
 @params(
-    {'arg': 'email', 'type': str, 'required': True, 'description': 'Guest email'},
+    {'arg': 'project', 'type': str, 'required': True, 'description': 'Project'},
+    {'arg': 'email', 'type': str, 'required': True, 'description': 'Email'},
 )
-def do_put(email, **kwargs):
+def do_put(email,project, **kwargs):
     """
     Save email from subscriber
     """
@@ -39,7 +40,7 @@ def do_put(email, **kwargs):
         return base_common.msg.error(amsgs.ALREADY_EXIST)
 
     id = sequencer().new('b')
-    query = "INSERT INTO subscribers (id, email) VALUES('{}','{}')".format(id, email)
+    query = "INSERT INTO subscribers (id, email, project) VALUES('{}','{}')".format(id, email, project)
     try:
         dbc.execute(query)
     except IntegrityError as e:
